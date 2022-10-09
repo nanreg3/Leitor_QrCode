94839054ed3dd439b11dd7aa7e57036cec90e0da
nanreg3 <nanreg3@gmail.com>
2022-10-09 06:16:37 -0300

    inserção de foto ao autenticar

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { LogService } from '../services/log.service';
 import { OperationResponse } from '../models/response.model';
 import { Appointment } from '../models/appointment.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -15,8 +16,14 @@ export class HomeComponent implements OnInit {
   private information: string = "Nenhuma informação de código detectada. Amplie um código QR para digitalizar.";
 
   public numeroQrCode: any;
+  public numeroCartao: any;
+  public pin: any;
+  public foto: any;
 
-  constructor(private logService: LogService, private cd: ChangeDetectorRef) {
+  constructor(
+    private http: HttpClient,
+    private logService: LogService,
+    private cd: ChangeDetectorRef) {
   }
 
   ngOnInit() {
@@ -27,6 +34,19 @@ export class HomeComponent implements OnInit {
     this.information = "Aguarde recuperando informações... ";
 
     this.numeroQrCode = $event;
+    var teste = this.numeroQrCode.split("@@");
+    this.numeroCartao = teste[0].substr(teste[0].length - 10);
+
+    this.http.get(`https://desktop-drttrdq:14052/api/v1/Identificadores/${this.numeroCartao}`).subscribe(
+      (x: any) => {
+        this.pin = x.pin;
+        this.http.get(`https://desktop-drttrdq:14052/api/v1/Fotos/base64?id=${x.pin}`).subscribe(
+          (x: any) => this.foto = x,
+          (e)=> console.error(e)
+        );
+      },
+      (e) => console.error(e)
+    )
 
     //const appointment = new Appointment($event);
     //this.logService.logAppointment(appointment).subscribe(
